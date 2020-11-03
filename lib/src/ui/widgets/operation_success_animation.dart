c451bf69d397d3f9c3c5640bfc880d9e38e3d3b3
Erkan Maraş <erkanm@nebim.com.tr>
2020-11-03 17:55:58 +0300

    OperationSuccessAnimation colors

--- lib/src/ui/widgets/operation_success_animation.dart
+++ lib/src/ui/widgets/operation_success_animation.dart
@@ -1,14 +1,21 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:aff/ui.dart';
+
+// ignore: must_be_immutable
 class OperationSuccessAnimation extends StatefulWidget {
+  OperationSuccessAnimation({this.borderColor, this.color, this.iconColor, this.shadowColor});
+
+  final Color borderColor;
+  final Color color;
+  final Color iconColor;
+  final Color shadowColor;
+
   @override
-  _OperationSuccessAnimationState createState() =>
-      _OperationSuccessAnimationState();
+  _OperationSuccessAnimationState createState() => _OperationSuccessAnimationState();
 }
 
-class _OperationSuccessAnimationState extends State<OperationSuccessAnimation>
-    with SingleTickerProviderStateMixin {
+class _OperationSuccessAnimationState extends State<OperationSuccessAnimation> with SingleTickerProviderStateMixin {
   AppTheme appTheme;
   AnimationController _animationController;
   Animation<double> doneTween;
@@ -57,26 +64,27 @@ class _OperationSuccessAnimationState extends State<OperationSuccessAnimation>
             child: Container(
               padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
-                  color: Color(0xff8ade98),
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 0.5),
-                        color: Colors.grey[400],
-                        blurRadius: max<double>(10 * tweenValue, 2),
-                        spreadRadius: max<double>(4 * tweenValue, 0.5))
-                  ]),
+                color: widget.color ?? Color(0xff8ade98),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 0.5),
+                      color: widget.shadowColor ?? Colors.grey[400],
+                      blurRadius: max<double>(10 * tweenValue, 2),
+                      spreadRadius: max<double>(4 * tweenValue, 0.5))
+                ],
+              ),
               child: Container(
                 height: 125 + (50 * tweenValue),
                 width: 125 + (50 * tweenValue),
                 decoration: BoxDecoration(
-                  color: appTheme.colors.success,
+                  color: widget.color ?? appTheme.colors.success,
                   shape: BoxShape.circle,
                 ),
                 child: Icon(
                   Icons.check,
                   size: 110 + (50 * tweenValue),
-                  color: Colors.white,
+                  color: widget.iconColor ?? Colors.white,
                 ),
               ),
             ),
