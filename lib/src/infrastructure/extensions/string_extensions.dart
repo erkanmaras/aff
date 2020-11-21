7fc94afe34f9ade1b35a08f7032c9dceea978b79
Erkan Maraş <erkanm@nebim.com.tr>
2020-11-21 15:00:52 +0300

    fix isdigit

--- lib/src/infrastructure/extensions/string_extensions.dart
+++ lib/src/infrastructure/extensions/string_extensions.dart
@@ -5,7 +5,7 @@ extension StringExtensions on String {
 
   bool isNullOrWhiteSpace() => this == null || isEmpty || trim() == '';
 
-  bool isDigit() => !isNullOrEmpty() || codeUnitAt(0) ^ 0x30 <= 9;
+  bool isDigit() => codeUnitAt(0) ^ 0x30 <= 9;
 
   bool equalsIgnoreCase(String value) =>
       (this == null && value == null) || (this != null && value != null && toLowerCase() == value.toLowerCase());
@@ -13,5 +13,4 @@ extension StringExtensions on String {
   bool containsIgnoreCase(String value) => this != null && value != null && toLowerCase().contains(value.toLowerCase());
 
   int compareIgnoreCase(String value) => toLowerCase().compareTo(value.toLowerCase());
-
 }
